781c04d4d3718766fb023349dfccba76b6183739
fuongtd <fuongtd@gmail.com>
2025-01-10 18:26:31 +0000

    Update

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-2025-01-10 17:16:33 begin
-0th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+2025-01-10 18:26:23 begin
+0th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
 1th Call to https://graph.microsoft.com/v1.0/me/?$select=displayName,skills - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
-4th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
-6th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
-7th Call to https://graph.microsoft.com/v1.0/users - Status code: 200
-8th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
-9th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
-10th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
-11th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
+4th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
+7th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
+8th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
+9th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
+10th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
