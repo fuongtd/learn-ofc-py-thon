d35998ae08255d1920486631911a61bfcf6f841a
fuongtd <fuongtd@gmail.com>
2025-02-19 10:20:22 +0000

    Update

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-2025-02-19 09:20:05 begin
-0th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
-1th Call to https://graph.microsoft.com/v1.0/me/drive/root - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
-4th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
-5th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/me/?$select=displayName,skills - Status code: 200
-7th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
-8th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
+2025-02-19 10:20:18 begin
+0th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
+1th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
+4th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
+7th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
