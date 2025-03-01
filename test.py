4ed9846ec94735a8d99eddfd3978984038532981
fuongtd <fuongtd@gmail.com>
2025-03-01 12:35:31 +0000

    Update

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-2025-03-01 11:14:31 begin
-0th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
-1th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
+2025-03-01 12:35:26 begin
+0th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
+1th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
 4th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
-7th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/users - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
+7th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
