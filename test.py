05e5ac73d9731771257767b73ceef5f9aacd2529
fuongtd <fuongtd@gmail.com>
2025-04-07 06:30:25 +0000

    Update

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-2025-04-07 05:20:55 begin
-0th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
-1th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
-4th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/me/?$select=displayName,skills - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/users - Status code: 200
-7th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
-8th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
-9th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
-10th Call to https://graph.microsoft.com/v1.0/me/drive/root - Status code: 200
+2025-04-07 06:30:17 begin
+0th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
+1th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+4th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
+5th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
+7th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
+8th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
+9th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
+10th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
+11th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
+12th Call to https://graph.microsoft.com/v1.0/me/?$select=displayName,skills - Status code: 200
