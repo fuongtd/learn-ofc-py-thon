95fe7a36c5367f0500454ca2f618e66539678c2c
fuongtd <fuongtd@gmail.com>
2025-05-03 12:38:56 +0000

    Update

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-2025-05-03 11:16:21 begin
-0th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
-1th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
+2025-05-03 12:38:48 begin
+0th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+1th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/me/drive/root - Status code: 200
 4th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/me/?$select=displayName,skills - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
-7th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
+7th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
+8th Call to https://graph.microsoft.com/v1.0/me/?$select=displayName,skills - Status code: 200
+9th Call to https://graph.microsoft.com/v1.0/users - Status code: 200
+10th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
+11th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
+12th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
+13th Call to https://api.powerbi.com/v1.0/myorg/apps - Status code: 403
