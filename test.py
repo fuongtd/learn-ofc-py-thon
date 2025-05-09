22be88f399c307e47ef553d16e392c00198db7cf
fuongtd <fuongtd@gmail.com>
2025-05-09 07:21:00 +0000

    Update

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-2025-05-09 06:31:03 begin
-0th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
-1th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
-4th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
-7th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
+2025-05-09 07:20:56 begin
+0th Call to https://graph.microsoft.com/v1.0/me/drive/root - Status code: 200
+1th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
+4th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+6th Call to https://api.powerbi.com/v1.0/myorg/apps - Status code: 403
+6th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
