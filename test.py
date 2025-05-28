5a3df3aa09125befb8faebe2d19c433c7fc59a1a
fuongtd <fuongtd@gmail.com>
2025-05-28 01:49:22 +0000

    Update

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-2025-05-27 23:19:32 begin
-0th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
-1th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
-4th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
-7th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
+2025-05-28 01:49:16 begin
+0th Call to https://graph.microsoft.com/v1.0/me/?$select=displayName,skills - Status code: 200
+1th Call to https://graph.microsoft.com/v1.0/users - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
+4th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/me/drive/root - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/drive/root - Status code: 200
+7th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
