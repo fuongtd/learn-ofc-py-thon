2ab7f8943b09bd64fd46c5f07bf5d972e64b8f4e
fuongtd <fuongtd@gmail.com>
2025-06-27 09:24:10 +0000

    Update

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-2025-06-27 08:29:58 begin
-0th Call to https://api.powerbi.com/v1.0/myorg/apps - Status code: 403
+2025-06-27 09:24:07 begin
 0th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
-1th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
-4th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
+1th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+3th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
+4th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
+7th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
