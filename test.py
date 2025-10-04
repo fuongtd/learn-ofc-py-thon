5d4a7fa06384dd89abf2da16226f008a5a46dc8c
fuongtd <fuongtd@gmail.com>
2025-10-04 21:16:28 +0000

    Update

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-2025-10-04 20:19:58 begin
-0th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
-1th Call to https://api.powerbi.com/v1.0/myorg/apps - Status code: 403
-1th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
-2th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
-4th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/me/drive/root/children - Status code: 200
-7th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
+2025-10-04 21:16:23 begin
+0th Call to https://api.powerbi.com/v1.0/myorg/apps - Status code: 403
+0th Call to https://graph.microsoft.com/beta/me/outlook/masterCategories - Status code: 200
+1th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
+2th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
+4th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/me/outlook/masterCategories - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/me/drive - Status code: 200
+7th Call to https://graph.microsoft.com/v1.0/sites/root - Status code: 200
