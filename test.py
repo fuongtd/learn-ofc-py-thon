ce97d65911201b85919f940cf4841ccd1c1640b1
fuongtd <fuongtd@gmail.com>
2026-08-02 10:07:48 +0000

    Update

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-2026-07-21 15:47:55 begin
-0th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
-1th Call to https://graph.microsoft.com/v1.0/me/?$select=displayName,skills - Status code: 200
-2th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
-3th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
-4th Call to https://graph.microsoft.com/v1.0/me/drive/root - Status code: 200
-5th Call to https://graph.microsoft.com/v1.0/me/mailFolders - Status code: 200
-6th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
+2026-08-02 10:07:42 begin
+0th Call to https://graph.microsoft.com/v1.0/me/drive/root - Status code: 200
+1th Call to https://graph.microsoft.com/v1.0/sites/root/lists - Status code: 200
+2th Call to https://graph.microsoft.com/v1.0/applications?$count=true - Status code: 200
+3th Call to https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules - Status code: 200
+4th Call to https://graph.microsoft.com/beta/me/messages?$select=internetMessageHeaders&$top=1 - Status code: 200
+5th Call to https://graph.microsoft.com/v1.0/me/messages - Status code: 200
+6th Call to https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta - Status code: 200
 7th Call to https://graph.microsoft.com/v1.0/sites/root/drives - Status code: 200
